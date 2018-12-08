cb71c6b45229baebb5f59018af2de10d159fb76f
Adam Lancaster <arglanc@users.noreply.github.com>
2018-12-08 18:42:42 +0000

    gsdgsdfg

--- LayoutService/Repositories/AppSubLinkRepository.cs
+++ LayoutService/Repositories/AppSubLinkRepository.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AberFitnessLayout.Models;
 using LayoutService.Models;
 using Microsoft.EntityFrameworkCore;
 
