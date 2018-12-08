14c9f6ae871cba10025bf6e9c5d635987eb96bd0
Adam Lancaster <arglanc@users.noreply.github.com>
2018-12-08 14:25:33 +0000

    refactor models, add access levels

--- AberFitnessLayout/Models/AppSubLink.cs
+++ AberFitnessLayout/Models/AppSubLink.cs
@@ -1,24 +1,15 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace AberFitnessLayout.Models
 {
-    public class AppLink
+    public class AppSubLink
     {
-        public AppLink()
-        {
-            SubLinks = new List<AppSubLink>();
-        }
-
         [Required]
         public virtual string DisplayName { get; set; }
 
         [Required]
         public virtual Uri Uri { get; set; }
-
-        [Required]
-        public virtual IList<AppSubLink> SubLinks { get; set; }
 
         [Required]
         public virtual AppLinkAccessLevel AccessLevel { get; set; }
