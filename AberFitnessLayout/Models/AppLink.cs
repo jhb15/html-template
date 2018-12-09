fd44b75e0dfa2ce0ec06cb9b701f724c9ac8a3a0
Adam Lancaster <arglanc@users.noreply.github.com>
2018-12-09 20:06:13 +0000

    fix cyclic dependencies

---
+++ AberFitnessLayout/Models/AppLink.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LayoutService.Models
+{
+    public class AppLink
+    {
+        public AppLink()
+        {
+            SubLinks = new List<AppSubLink>();
+        }
+
+        [Key]
+        public virtual int Id { get; set; }
+
+        [Required]
+        public virtual string IconName { get; set; }
+
+        [Required]
+        public virtual string DisplayName { get; set; }
+
+        [Url]
+        public virtual string Uri { get; set; }
+
+        [Required]
+        public virtual IList<AppSubLink> SubLinks { get; set; }
+
+        [Required]
+        public virtual AppLinkAccessLevel AccessLevel { get; set; }
+    }
+}
