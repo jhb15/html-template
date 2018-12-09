196ae958f5d7b902437c36f31e767f1451688a02
Adam Lancaster <arglanc@users.noreply.github.com>
2018-12-09 20:26:54 +0000

    please work

--- AberFitnessLayout/Models/AppLink.cs
+++ AberFitnessLayout/Models/AppLink.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace LayoutService.Models
+namespace AberFitnessLayout.Models
 {
     public class AppLink
     {
--- AberFitnessLayout/Models/AppSubLink.cs
+++ AberFitnessLayout/Models/AppSubLink.cs
@@ -1,7 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace LayoutService.Models
+namespace AberFitnessLayout.Models
 {
     public class AppSubLink
     {
