6d01015d3967201091b59b6fc21a9b2ef8047e6b
Adam Lancaster <arglanc@users.noreply.github.com>
2018-12-11 14:48:53 +0000

    Update models

--- LayoutService/Models/AppSubLink.cs
+++ LayoutService/Models/AppSubLink.cs
@@ -9,19 +9,27 @@ namespace LayoutService.Models
         public virtual int Id { get; set; }
 
         [Required]
+        [Display(Name = "FontAwesome Icon")]
         public virtual string IconName { get; set; }
 
         [Required]
+        [Display(Name = "Name")]
         public virtual string DisplayName { get; set; }
 
         [Required]
-        [Url]
+        [Display(Name = "URI")]
         public virtual string Uri { get; set; }
 
         [Required]
+        [Display(Name = "Access Level")]
         public virtual AppLinkAccessLevel AccessLevel { get; set; }
 
         [Required]
         public virtual int AppLinkId { get; set; }
+
+        [Required]
+        [Range(0, 10000)]
+        [Display(Name = "Link Priority")]
+        public virtual int Priority { get; set; }
     }
 }
