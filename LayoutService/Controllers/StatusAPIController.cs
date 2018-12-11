743ce4e6309c7a223eefe1903b56122829b6ef9a
Dan Monaghan <dan@daniel-monaghan.co.uk>
2018-12-11 15:46:54 +0000

    - Adds /api/status endpoint

---
+++ LayoutService/Controllers/StatusAPIController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutService.Controllers
+{
+    [Route("api/status")]
+    [ApiController]
+    public class StatusAPIController : ControllerBase
+    {
+        // GET: /api/status
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
